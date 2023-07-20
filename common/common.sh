edbe7eea3cfc57de0f8bebf3a241890f17ba2c14
mizhidajiangyou <670746945@qq.com>
2023-07-20 14:50:20 +0800

    update common

--- common/common.sh
+++ common/common.sh
@@ -13,40 +13,17 @@ function usage() {
   exit 1
 }
 
-# 通用日志管理
-function sendLog() {
-  local LEVEL="INFO - "
-  case $2 in
-  0)
-    if [ "${LOG_LEVEL}" == "debug" ]; then
-      LEVEL="DEBUG - "
-    else
-      return
-    fi
-    ;;
-  1)
-    LEVEL="INFO - "
-    ;;
-  2)
-    LEVEL="WARNING - "
-    ;;
-  3)
-    LEVEL="ERROR - "
-    ;;
-  4)
-    LEVEL="CRITICAL - "
-    LOG_CONSOLE_PRINT="true"
-    ;;
-  *)
-    LEVEL="INFO - "
-    ;;
-  esac
+### 字符串处理 ###
+# 小写
+function toLower() {
+  local str="$1"
+  echo "$str" | tr '[:upper:]' '[:lower:]'
+}
 
-  printf "%-25s%s\n" "$(date '+%Y-%m-%d %H:%M:%S.%3N')" " ${LEVEL}$1" >>"${LOG_FILE}"
-
-  if [ "${LOG_CONSOLE_PRINT}" == "true" ]; then
-    printf "%-25s%s\n" "$(date '+%Y-%m-%d %H:%M:%S.%3N')" " ${LEVEL}$1"
-  fi
+# 大写
+function toUpper() {
+  local str="$1"
+  echo "$str" | tr '[:lower:]' '[:upper:]'
 }
 
 # 格式化
@@ -55,6 +32,9 @@ function trim() {
   local trimmed=${trimmed## }
   echo "$trimmed"
 }
+
+
+### 配置模块 ###
 
 # 获取配置 section key file values
 function configParser() {
@@ -129,6 +109,8 @@ function readConfig() {
     fi
   done <"${config_file}"
 }
+
+### 文件确认模块 ###
 
 # 进入指定目录，如果不存在则创建
 function intoPath() {
@@ -265,5 +247,82 @@ function zLock() {
     ;;
   esac
 }
+
+### 随机数模块 ###
+# 取范围内随机数，位数不能超过9
+function urandomIntInLim() {
+  local min=$1
+  local max=$2
+  local length=${3:-5} # 如果未指定，则默认为 20
+
+  if (( min > max )); then
+    echo 0
+  fi
+
+  local num
+  num=$(od -An -N"${length}" -tu4 /dev/urandom | tr -d ' ')
+  num=$((num % (max - min + 1) + min))
+
+  echo "$num"
+}
+
+# 生成随机位数的整数
+function urandomInt() {
+  local ur_length="${1:-3}"
+
+  num=$(head /dev/urandom -n 10000 | tr -dc 0-9 | head -c "${ur_length}")
+  # 去掉0开头的
+  awk -v a="${num}" -v b=1 'BEGIN{print a+b}'
+}
+
+# 随机长度的字符串
+function urandomStr() {
+  local ur_length="${1:-20}"
+
+  # 生成随机字符串
+  local str
+  str=$(head -c "$((ur_length * 2))" /dev/urandom | base64 | tr -d '/+' | head -c "$ur_length")
+
+  # 输出结果
+  echo "$str"
+}
+
+### 日志管理 ###
+# 通用日志管理
+function sendLog() {
+  local LEVEL="INFO - "
+  case $2 in
+  0)
+    if [ "${LOG_LEVEL}" == "debug" ]; then
+      LEVEL="DEBUG - "
+    else
+      return
+    fi
+    ;;
+  1)
+    LEVEL="INFO - "
+    ;;
+  2)
+    LEVEL="WARNING - "
+    ;;
+  3)
+    LEVEL="ERROR - "
+    ;;
+  4)
+    LEVEL="CRITICAL - "
+    LOG_CONSOLE_PRINT="true"
+    ;;
+  *)
+    LEVEL="INFO - "
+    ;;
+  esac
+
+  printf "%-25s%s\n" "$(date '+%Y-%m-%d %H:%M:%S.%3N')" " ${LEVEL}$1" >>"${LOG_FILE}"
+
+  if [ "${LOG_CONSOLE_PRINT}" == "true" ]; then
+    printf "%-25s%s\n" "$(date '+%Y-%m-%d %H:%M:%S.%3N')" " ${LEVEL}$1"
+  fi
+}
+
 
 readConfig "${SHELL_HOME}global.cfg"
