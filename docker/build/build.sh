d34f4f01260964972658bf28442316f0ff656372
mizhidajiangyou <670746945@qq.com>
2024-03-11 16:45:30 +0800

    add cicd

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -4,10 +4,13 @@ source "${SHELL_HOME}"common/common.sh
 
 function build_docker_then_push() {
   local tag=$1 image
-  image="mzdjy/visualization:$tag-auto"
+  image="mzdjy/build:$tag-auto"
   sendLog "do build and push $image." 0
+  pushd "$tag"  || exit 1
   docker build . -t "${image}"
   docker push "${image}"
+  popd || exit 1
 }
-
-multiProcess build_docker_then_push "$(ls docker)"
+cd "$SHELL_HOME/docker/build/docker" || exit 1
+# shellcheck disable=SC2010
+multiProcess build_docker_then_push "$(ls |grep -v '.sh')"
