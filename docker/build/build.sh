7c33b75d8f26e7c8d16c895022d49c06ea8ea249
mizhidajiangyou <670746945@qq.com>
2024-07-08 16:50:26 +0800

    rebuild cd

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -6,10 +6,10 @@ function build_docker_then_push() {
   local tag=$1 image
   image="mzdjy/build:$tag-auto"
   sendLog "do build and push $image." 0
-  pushd "$tag"  || exit 1
+  cd "$tag"  || exit 1
   docker build . -t "${image}"
   docker push "${image}"
-  popd || exit 1
+  cd - || exit 1
 }
 cd "${SHELL_HOME}docker/build/docker" || exit 1
 # shellcheck disable=SC2010
