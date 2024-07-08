7f56298afaf3fe06a06f3950ad84e1179e6c213e
mizhidajiangyou <670746945@qq.com>
2024-07-08 17:44:57 +0800

    修复 set +x 导致多进程函数一定会执行出错的问题

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
 source "${SHELL_HOME}"common/common.sh
-set -xo pipefail
+
 function build_docker_then_push() {
   local tag=$1 image
   image="mzdjy/build:$tag-auto"
   sendLog "do build and push $image." 0
   pushd "$tag"  || exit 1
-  docker build . -t "${image}" 1&>/dev/null
-  docker push "${image}" 1&>/dev/null
+  docker build . -t "${image}" -q
+  docker push "${image}"
   popd  || exit 1
 }
 cd "${SHELL_HOME}docker/build/docker" || exit 1
-set +x
 # shellcheck disable=SC2010
 multiProcess build_docker_then_push "$(ls |grep -v '.sh')"
