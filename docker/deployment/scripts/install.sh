5a8770718ddc286fe819b2406e55d6bfa2fef7ea
mizhidajiangyou <670746945@qq.com>
2025-02-28 14:50:00 +0800

    add function init_config_network

--- docker/deployment/scripts/install.sh
+++ docker/deployment/scripts/install.sh
@@ -14,6 +14,27 @@ function init_docker_network() {
 
 }
 
+function init_config_network() {
+   sendLog "init network config.."
+   # shellcheck disable=SC2091
+   if $(configParser network auto_config images.cfg) ;then
+     # shellcheck disable=SC2155
+     local net_name="$(configParser network network images.cfg)"
+     sendLog "auto config network..."
+     if [ "$net_name" == "eth0" ]; then
+       ip_list=$(ip a | grep 'state UP' | grep -v noqueue | awk '{gsub(":", "", $2);print $2}')
+       # 当前可用的网卡
+       ip=$(ifconfig "${ip_list[0]}" | grep broadcast | awk 'NR=1{print $2}')
+       sendLog "used ${ip_list[0]} , address $ip"
+       # 修改配置中的网络
+       configParser network network images.cfg "${ip_list[0]}"
+       configParser network local_ip images.cfg "$ip"
+     else
+       echo "Configuration has been modified, skipping automatic configuration..."
+     fi
+    fi
+}
+
 function do_install() {
   local ser
   ser=$1
@@ -30,6 +51,7 @@ function do_install() {
 
 function main() {
   init_docker_network
+  init_config_network
   for_service_do do_install
 }
 
