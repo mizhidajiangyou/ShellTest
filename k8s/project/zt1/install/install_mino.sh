0d4228593d7f0d04b7e833468ec1cf059ddaae62
mizhidajiangyou <670746945@qq.com>
2025-07-31 14:49:25 +0800

    add minio

---
+++ k8s/project/zt1/install/install_mino.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+source scripts/common.sh
+
+service_name="minio"
+# 基于：https://github.com/prometheus-community/helm-charts/releases/download/prometheus-20.0.2/prometheus-20.0.2.tgz
+
+setKubeConfig
+
+# yaml_create 创建yaml
+# yaml_apply 依赖执行yaml_create后 kubectl apply
+# helm_apply 执行helm install
+# helm_upgrade 执行helm upgrade
+# yaml_delete 依赖执行yaml_create后 kubectl delete
+helm_mode=$1
+helm_cmd="helm install"
+yaml_dir=""
+namespace="$(getImagesConf 'k8s' 'namespace')"
+
+if [ -z "${namespace}" ]; then
+  sendLog "namespace is null! please check images.cfg!" 3
+  exit 1
+fi
+
+if [ "${helm_mode}" == "helm_upgrade" ]; then
+  helm_cmd="helm upgrade"
+elif [ "${helm_mode}" == "yaml_create" ] || [ "${helm_mode}" == "yaml_apply" ]; then
+  helm_cmd="helm template"
+  yaml_dir="--output-dir yaml"
+fi
+
+sendLog "Do $helm_cmd $service_name ..." 1
+
+if [ "${helm_mode}" == "yaml_apply" ]; then
+  if [ ! -d yaml ]; then
+    sendLog "please create yaml first !" 3
+    exit 1
+  fi
+  yamlApply "$service_name" "${namespace}"
+elif [ "${helm_mode}" == "yaml_delete" ]; then
+  if [ ! -d yaml ]; then
+    sendLog "please create yaml first !" 3
+    exit 1
+  fi
+  yamlDelete "$service_name" "${namespace}"
+else
+  # 配置
+  conf_cmd="\
+  --set image.registry="" \
+  --set image.repository=$(getImagesPart $service_name 'image' 1) \
+  --set image.tag=$(getImagesPart $service_name 'image' 2) \
+  --set server.replicaCount=$(getImagesConf $service_name 'replica_count') \
+  --set auth.rootPassword=$(getImagesConf $service_name 'password') \
+  --set auth.rootUser=$(getImagesConf $service_name 'user') \
+  --set persistence.size=$(getImagesConf $service_name 'size') \
+  --set server.ingress.enabled=false \
+  $service_name --namespace ${namespace} ${yaml_dir} artifact/$service_name"
+
+  if eval "$helm_cmd $conf_cmd"; then
+    sendLog "Do $helm_cmd $service_name successful!" 1 g
+  else
+    sendLog "Do $helm_cmd $service_name  failed!" 3
+    exit 1
+  fi
+fi
